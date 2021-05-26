3dd32e2d752118b9e8cfc8b4f62dea9e495a07f3
mdsohelranaahc793 <84516385+mdsohelranaahc793@users.noreply.github.com>
2021-05-26 21:40:04 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+confirm('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+alert('There’s a surprise for you. Pls press ok.')
+
+$('.hack-text').show();
